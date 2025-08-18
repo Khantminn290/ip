96e7fa08ed297cc1fcb253015454cd86a8923966
KaungKhantMinn <khantminn171@gmail.com>
2025-08-19 00:00:24 +0800

    Renamed file to Ronaldo.java

---
+++ src/main/java/Ronaldo.java
@@ -0,0 +1,11 @@
+public class Ronaldo {
+    public static void main(String[] args) {
+        String greeting = "____________________________________________________________\n"
+               + " Hello! I'm Ronaldo\n" +
+                " What can I do for you? Siuu!\n" +
+                "____________________________________________________________\n"
+               + " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(greeting);
+    }
+}
