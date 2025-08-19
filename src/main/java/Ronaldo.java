dd256f5d6ad84ff3635c4a2a6acd0eeaecc98d9b
KaungKhantMinn <khantminn171@gmail.com>
2025-08-19 20:21:27 +0800

    Added Mark as Done feature

--- src/main/java/Ronaldo.java
+++ src/main/java/Ronaldo.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Ronaldo {
-    ArrayList<String> list = new ArrayList<>();
+    ArrayList<Task> list = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
 
     private String greeting = " Hello! I'm Ronaldo\n"
@@ -24,6 +24,7 @@ public class Ronaldo {
         System.out.println(encase(this.bye));
     }
 
+    // addTask contains mark and unmark feature from user input
     public void addTask() {
         String input = "";
         while(!input.equals("bye")) {
@@ -33,19 +34,41 @@ public class Ronaldo {
                 break;
             } else if (input.equals("list")) {
                 this.printTask();
+            } else if (input.contains("mark") ) {
+                if (input.contains("unmark")) {
+                    String[] parts = input.split(" ");
+                    String numbrStr = parts[1];
+                    int number = Integer.parseInt(numbrStr);
+                    Task unmarkTask = this.list.get(number - 1);
+                    unmarkTask.unmark();
+                    System.out.println(encase("OK, I've marked this task as not done yet:\n" + unmarkTask));
+                } else {
+                    String[] parts = input.split(" ");
+                    String numbrStr = parts[1];
+                    int number = Integer.parseInt(numbrStr);
+                    Task markedTask = this.list.get(number - 1);
+                    markedTask.markAsDone();
+                    System.out.println(encase("Nice! I've marked this task as done:\n " + markedTask));
+                }
             } else {
                 System.out.println(encase("added: " + input));
-                this.list.add(input);
+                this.list.add(new Task(input));
             }
         }
         scanner.close();
     }
 
+
     public void printTask() {
         int size = this.list.size();
+        StringBuilder tasks = new StringBuilder();
         for (int i = 0; i < size; i++) {
-            System.out.println(encase(String.format("%d. %s", i+1, this.list.get(i))));
+            tasks.append(String.format("%d. %s", i + 1, this.list.get(i)));
+            if (i < size - 1) {
+                tasks.append("\n");
+            }
         }
+        System.out.println(encase("Here are the tasks in your list: \n" + tasks));
     }
 
     public void echo() {
@@ -62,6 +85,7 @@ public class Ronaldo {
         scanner.close();
 
     }
+
     public static void main(String[] args) {
         Ronaldo ronaldo = new Ronaldo();
         ronaldo.greet();
