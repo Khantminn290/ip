47fb08c1c2947288f9e59807a2e165e2209d693f
KaungKhantMinn <khantminn171@gmail.com>
2025-08-19 19:08:28 +0800

    Added support for echo

--- src/main/java/Ronaldo.java
+++ src/main/java/Ronaldo.java
@@ -1,11 +1,43 @@
+import java.util.Scanner;
+
 public class Ronaldo {
+    private String greeting = " Hello! I'm Ronaldo\n"
+            + " What can I do for you? Siuu!";
+
+    private String bye = " Bye. Hope to see you again soon!";
+
+    public static String encase(String s) {
+        return "____________________________________________________________\n"
+                + String.format("%s\n", s)
+                + "____________________________________________________________\n";
+    }
+
+    public void greet() {
+        System.out.println(encase(this.greeting));
+    }
+
+    public void signOff() {
+        System.out.println(encase(this.bye));
+    }
+
+    public void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String input =  "";
+        while(!input.equals("bye")) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                this.signOff();
+                break;
+            } else {
+                System.out.println(encase(input));
+            }
+        }
+        scanner.close();
+
+    }
     public static void main(String[] args) {
-        String greeting = "____________________________________________________________\n"
-               + " Hello! I'm Ronaldo\n" +
-                " What can I do for you? Siuu!\n" +
-                "____________________________________________________________\n"
-               + " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
-        System.out.println(greeting);
+        Ronaldo ronaldo = new Ronaldo();
+        ronaldo.greet();
+        ronaldo.echo();
     }
 }
