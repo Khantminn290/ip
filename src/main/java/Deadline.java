d31efe553cbc2328b4cc7a3a4df97807bc7cb1f9
KaungKhantMinn <khantminn171@gmail.com>
2025-08-25 20:24:43 +0800

    Update LocalDate storing method

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -4,33 +4,19 @@ import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task {
     protected String by;
+    protected LocalDate date;
+    protected LocalTime time;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+
+        // Expecting format: "yyyy-MM-dd HHmm"
+        String[] dateTime = by.split(" ");
+        this.date = LocalDate.parse(dateTime[0], DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        this.time = LocalTime.parse(dateTime[1], DateTimeFormatter.ofPattern("HHmm"));
     }
 
-    public String getDate() {
-        try {
-            // split input into date and time
-            String[] date_time = this.by.split(" ");
-
-            // parse date (assume format yyyy-MM-dd)
-            LocalDate date = LocalDate.parse(date_time[0], DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-
-            // parse time (assume format HHmm, e.g. 1800)
-            LocalTime time = LocalTime.parse(date_time[1], DateTimeFormatter.ofPattern("HHmm"));
-
-            // format
-            String dateFormatted = date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
-            String timeFormatted = time.format(DateTimeFormatter.ofPattern("hh:mm a"));
-
-            return dateFormatted + ", " + timeFormatted;
-
-        } catch (Exception e) {
-            return this.by; // fallback if parsing fails
-        }
-    }
     @Override
     public String toString() {
         return "[D]" + super.toString() + "(by:" + by + ")";
