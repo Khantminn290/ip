20f1875b64208af97f703ac3c16e0e2f6b199d6d
KaungKhantMinn <khantminn171@gmail.com>
2025-08-27 10:22:41 +0800

    Add JUnit tests for Task package

---
+++ src/test/java/ronaldo/task/TaskTest.java
@@ -0,0 +1,37 @@
+package ronaldo.task;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskTest {
+
+    @Test
+    public void testConstructor_initialValues() {
+        Task task = new Task("Read book");
+        assertEquals("Read book", task.getDescription());
+        assertFalse(task.isDone());
+        assertEquals(" ", task.getStatusIcon());
+        assertEquals("[ ] Read book", task.toString());
+    }
+
+    @Test
+    public void testMarkAsDone() {
+        Task task = new Task("Do homework");
+        task.markAsDone();
+        assertTrue(task.isDone());
+        assertEquals("X", task.getStatusIcon());
+        assertEquals("[X] Do homework", task.toString());
+    }
+
+    @Test
+    public void testUnmark() {
+        Task task = new Task("Go jogging");
+        task.markAsDone(); // mark first
+        assertTrue(task.isDone());
+
+        task.unmark(); // then unmark
+        assertFalse(task.isDone());
+        assertEquals(" ", task.getStatusIcon());
+        assertEquals("[ ] Go jogging", task.toString());
+    }
+}
